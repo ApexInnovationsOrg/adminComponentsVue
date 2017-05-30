9b678d84d06260fe7b96d4f005124b6688ccb4be
APEX\eddie <eddie>
2017-05-30 17:14:15 -0500

    not a lot of progress today. Got some bones and hit areas though.

--- src/leadPlacement.vue
+++ src/leadPlacement.vue
@@ -5,7 +5,7 @@
           {{ message }} - Question #{{selectedQuestion}}
           <div class="optionHolder">
             <select v-on:change="changeQuestion" v-model="selectedQuestion">
-                    <option v-for="option in questionOptions" :value="option">{{option}}</option>
+                    <option v-for="(option,key) in questionOptions" :value="key">{{key}} - {{ option.referral }}</option>
                       <!-- <organization-option v-for="(org, i) in orgs" :org="org" :key="org.ID"></organization-option> -->
                    </select>
 
@@ -14,10 +14,11 @@
                       <input type="checkbox"  v-model="selectedLeadOption" :value="leadOption"> {{ leadOption }} </input>
                     </div>
           </div>
-          <img class="mrMcMann" src="https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/Adam.png" />
+          <img class="mrMcMann" v-for="source in backgroundSource" :src="source" />
           <div class="leadHolder">
               <single-lead v-for="(lead, key) in placements" v-on:selectLead="filterUser" :title="lead.title" class="lead" :lead="lead"></single-lead>
           </div>  
+              <img class="hitAreas" v-for="area in hitAreas" :src="area" />
        </div>
        
       </div>
@@ -31,14 +32,16 @@ export default {
   name: 'leadComponentVue',
   data () {
     return {
+      backgroundSource:['https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/bedside/Front_Body.png'],
+      hitAreas:[],
       answers:[],
       placements:[],
       message:'Lead Placement',
       leadOptions:[],
       selectedLeads:[],
-      questionOptions:[],
+      questionOptions:{},
       selectedLeadOption:[],
-      selectedQuestion:0
+      selectedQuestion:0,
     }
   },
   watch:{
@@ -54,12 +57,14 @@ export default {
             if(this.answers[i].QuestionID == this.selectedQuestion)
             {
               this.answers[i].Placements[j].title = j;
+              this.answers[i].Placements[j].user = this.answers[i];
               this.placements.push(this.answers[i].Placements[j]);
               
             }
           }
         }
         this.filterLeads();
+        this.highlightHitAreas();
     },
     selectedLeadOption:function(){
       this.placements = [];
@@ -71,6 +76,7 @@ export default {
             if(this.answers[i].QuestionID == this.selectedQuestion && this.selectedLeadOption.indexOf(j) !== -1)
             {
               this.answers[i].Placements[j].title = j;
+              this.answers[i].Placements[j].user = this.answers[i];
               this.placements.push(this.answers[i].Placements[j]);
               
             }
@@ -83,6 +89,63 @@ export default {
       'options-box':optionsBox
   },
   methods:{
+    highlightHitAreas:function()
+    {
+      switch(this.selectedQuestion) {
+          case '2410':
+                this.backgroundSource = [
+                  'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/Front_Body.png',
+                  'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/Front_Ribs.png'
+                ]
+
+                this.hitAreas = [
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/LA.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/LL.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/RA.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/RL.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/V1.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/V2.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/V3.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/V4.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/V5.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/12Lead/V6.png'
+              ]
+
+              break;
+          case '2411':
+                this.backgroundSource = [
+                  'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/Front_Body.png',
+                  'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/Front_Ribs.png'
+                ]
+                this.hitAreas = [
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/LA.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/LL.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/RA.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/RL.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/V1R.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/V2R.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/V3R.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/V4R.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/V5R.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/diagnostic/18Lead/V6R.png'
+                ]
+              break;
+          case '2412':
+              this.backgroundSource = ['https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/bedside/Front_Body.png'];
+              this.hitAreas = [
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/bedside/3Lead/LA.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/bedside/3Lead/LL.png',
+                'https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/bedside/3Lead/RA.png'
+              ]
+              break;
+          default:
+              this.backgroundSource = ['https://d9de626265c75f8c35a5-ef69c638836b68114e46e3b19717493d.ssl.cf2.rackcdn.com/bedside/Front_Body.png'];
+              this.hitAreas = [];
+              break;
+      }
+
+      console.log(this.selectedQuestion);
+    },
     filterUser:function(dootdoot)
     {
       console.log('filter user');
@@ -123,16 +186,23 @@ export default {
     },
     getQuestions:function()
     {
-      var tempQuestions = [];
+      var tempQuestions ={};
       for(var i in this.answers)
       {
-        if(tempQuestions.indexOf(this.answers[i].QuestionID) == -1)
+        if(typeof tempQuestions[this.answers[i].QuestionID] == 'undefined')
         {
-          tempQuestions.push(this.answers[i].QuestionID);
+          /*let tempChoice = {
+            questionID: this.answers[i].QuestionID,
+            referral: this.answers[i].referral
+          }
+
+          tempQuestions.push(tempChoice);*/
+
+          tempQuestions[this.answers[i].QuestionID] = {referral:this.answers[i].Referral};
         }
       }
 
-      this.questionOptions = tempQuestions.sort();      
+      this.questionOptions = tempQuestions;      
     }
   },
     created: function() {
@@ -195,12 +265,15 @@ export default {
     height:100%;
     width:100%;
   }
-  .mrMcMann{
+  .mrMcMann, .hitAreas{
     position:absolute;
    height:2156px;
       width:1032px;
       top:0;
       left:0;
+  }
+  .hitAreas{
+    opacity:0.4;
   }
   .optionHolder{
 
